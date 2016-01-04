ef961eab1271cfeb0666caab8de22741d9b0e348
jp94 <jamespark@gatech.edu>
2016-01-04 17:50:04 -0500

    First refactor commit to use only two metadata. Potion effects are now working. playerList now uses Set from ArrayList.

--- src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIEAddCommand.java
+++ src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIEAddCommand.java
@@ -1,6 +1,8 @@
 package edu.gatech.at.jamespark.AdvancedItemEffects.commands;
 
 import edu.gatech.at.jamespark.AdvancedItemEffects.Effects;
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.EffectEffectsList;
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.PotionEffectsList;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -24,129 +26,119 @@ public class AIEAddCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command,
                              String label, String[] args) {
 
-        // Prevent executing from server console.
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED
                     + "Must execute this command as a player.");
             return false;
         }
+        if (!label.equalsIgnoreCase("aieadd")) return false;
+        Player player = (Player) sender;
+        if ((!player.hasPermission("AIE.add")) && (!player.isOp())) {
+            player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
+                    + " lacks \"AIE.add\" permission.");
+            return false;
+        }
+        if (player.getItemInHand().getItemMeta() == null) {
+            player.sendMessage(ChatColor.RED
+                    + "You must be holding an item!");
+            return false;
+        }
 
-        // Command: aieadd
-        if (label.equalsIgnoreCase("aieadd")) {
+        if (args.length == 0) {
+            String[] missingArgs = {
+                    ChatColor.RED + "Incomplete command input.",
+                    ChatColor.RED + "/aieadd [potion effect] [multiplier]",
+                    ChatColor.GREEN + PotionEffectsList.getString(),
+                    ChatColor.RED + "/aieadd [particle effect]",
+                    ChatColor.GREEN + EffectEffectsList.getString()
+            };
+            player.sendMessage(missingArgs);
+            return false;
+        }
 
-            Player player = (Player) sender;
+        ItemStack item = player.getItemInHand();
+        ItemMeta itemMeta = item.getItemMeta();
+        List<String> lore = itemMeta.getLore();
 
-            // Check necessary cases.
-            if ((!player.hasPermission("AIE.add")) && (!player.isOp())) {
-                player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
-                        + " lacks \"AIE.add\" permission.");
+        // if /aieadd [potion effect] [multiplier]
+        if (args.length > 1) {
+
+            if (!effects.stringArrayContainsIgnoreCase(
+                    PotionEffectsList.getArray(), args[0])) {
+                player.sendMessage(ChatColor.RED + "Wrong effect type.");
                 return false;
             }
-
-            if (player.getItemInHand().getItemMeta() == null) {
-                player.sendMessage(ChatColor.RED
-                        + "You must be holding an item!");
-                return false;
-            }
-
-            if (args.length == 0) {
-                String[] missingArgs = {
-                        ChatColor.RED + "Incomplete command input.",
-                        ChatColor.RED + "/aieadd [potion effect] [multiplier]",
-                        ChatColor.RED + "/aieadd [particle effect]"};
-                player.sendMessage(missingArgs);
-                return false;
-            }
-
-            // Initiate variables
-            ItemStack item = player.getItemInHand();
-            ItemMeta itemMeta = item.getItemMeta();
-            List<String> lore = itemMeta.getLore();
-
-            // if /aieadd [potion effect] [multiplier]
-            if (args.length > 1) {
-
-                if (!effects.stringArrayContainsIgnoreCase(
-                        effects.potionEffectsList, args[0])) {
-                    player.sendMessage(ChatColor.RED + "Wrong effect type.");
-                    return false;
-                }
-
-                // Checks for valid [multiplier]
-                try {
-                    int checkIfInt = Integer.parseInt(args[1]);
-                    if (checkIfInt < 1) {
-                        player.sendMessage("Multiplier must be between 1 and 2147483647.");
-                        return false;
-                    }
-                } catch (NumberFormatException e) {
+            // Checks for valid [multiplier]
+            try {
+                int checkIfInt = Integer.parseInt(args[1]);
+                if (checkIfInt < 1) {
                     player.sendMessage("Multiplier must be between 1 and 2147483647.");
                     return false;
                 }
-
-                if (lore == null) {
-                    itemMeta.setLore(Arrays.asList(ChatColor.GOLD + "Effects:"));
-                    lore = itemMeta.getLore();
-                }
-
-                if (!(lore.contains(ChatColor.GOLD + "Effects:"))) {
-                    lore.add(ChatColor.GOLD + "Effects:");
-                }
-
-                if (effects.listContainsIgnoreCase(lore, args[0]) != -1) {
-                    player.sendMessage(ChatColor.RED
-                            + "This effect type is already used.");
-                    return false;
-                }
-
-                lore.add("          " + ChatColor.BLUE + args[0].toUpperCase()
-                        + " " + ChatColor.WHITE + args[1]);
-
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                player.sendMessage(ChatColor.AQUA
-                        + "A potion effect has been added.");
-                effects.addAllBoundItemPotionEffects(player, lore);
-                return true;
-
-                // if /aieadd [particle effect]
-            } else if (args.length == 1) {
-
-                if (effects.stringArrayContainsIgnoreCase(
-                        effects.potionEffectsList, args[0])) {
-                    player.sendMessage(ChatColor.RED
-                            + "/aieadd [potion effect] [multiplier]");
-                    return false;
-                } else if (!effects.stringArrayContainsIgnoreCase(
-                        effects.particleEffectsList, args[0])) {
-                    player.sendMessage(ChatColor.RED + "Wrong effect type.");
-                    return false;
-                }
-
-                if (lore == null) {
-                    itemMeta.setLore(Arrays.asList(ChatColor.GOLD + "Effects:"));
-                    lore = itemMeta.getLore();
-                }
-
-                if (!(lore.contains(ChatColor.GOLD + "Effects:"))) {
-                    lore.add(ChatColor.GOLD + "Effects:");
-                }
-
-                if (effects.listContainsIgnoreCase(lore, args[0]) != -1) {
-                    player.sendMessage(ChatColor.RED
-                            + "This effect type is already used.");
-                    return false;
-                }
-
-                lore.add("          " + ChatColor.GREEN + args[0].toUpperCase());
-
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                player.sendMessage(ChatColor.AQUA
-                        + "A particle effect has been added.");
-                effects.addAllBoundItemParticleEffects(player, lore);
-                return true;
+            } catch (NumberFormatException e) {
+                player.sendMessage("Multiplier must be between 1 and 2147483647.");
+                return false;
             }
+            if (lore == null) {
+                itemMeta.setLore(Arrays.asList(ChatColor.GOLD + "Effects:"));
+                lore = itemMeta.getLore();
+            }
+            if (!(lore.contains(ChatColor.GOLD + "Effects:"))) {
+                lore.add(ChatColor.GOLD + "Effects:");
+            }
+            if (effects.listContainsIgnoreCase(lore, args[0]) != -1) {
+                player.sendMessage(ChatColor.RED
+                        + "This effect type is already used.");
+                return false;
+            }
+
+            lore.add("          " + ChatColor.BLUE + args[0].toUpperCase()
+                    + " " + ChatColor.WHITE + args[1]);
+
+            itemMeta.setLore(lore);
+            item.setItemMeta(itemMeta);
+            player.sendMessage(ChatColor.AQUA
+                    + "A potion effect has been added.");
+            effects.addAllBoundItemPotionEffects(player, lore);
+            return true;
+
+            // if /aieadd [particle effect]
+        } else if (args.length == 1) {
+
+            if (effects.stringArrayContainsIgnoreCase(
+                    PotionEffectsList.getArray(), args[0])) {
+                player.sendMessage(ChatColor.RED
+                        + "/aieadd [potion effect] [multiplier]");
+                return false;
+            } else if (!effects.stringArrayContainsIgnoreCase(
+                    EffectEffectsList.getArray(), args[0])) {
+                player.sendMessage(ChatColor.RED + "Wrong effect type.");
+                return false;
+            }
+
+            if (lore == null) {
+                itemMeta.setLore(Arrays.asList(ChatColor.GOLD + "Effects:"));
+                lore = itemMeta.getLore();
+            }
+
+            if (!(lore.contains(ChatColor.GOLD + "Effects:"))) {
+                lore.add(ChatColor.GOLD + "Effects:");
+            }
+
+            if (effects.listContainsIgnoreCase(lore, args[0]) != -1) {
+                player.sendMessage(ChatColor.RED
+                        + "This effect type is already used.");
+                return false;
+            }
+
+            lore.add("          " + ChatColor.GREEN + args[0].toUpperCase());
+
+            itemMeta.setLore(lore);
+            item.setItemMeta(itemMeta);
+            player.sendMessage(ChatColor.AQUA
+                    + "A particle effect has been added.");
+            effects.addAllBoundItemParticleEffects(player, lore);
+            return true;
         }
         return false;
     }
--- src/edu/gatech/at/jamespark/AdvancedItemEffects/Effects.java
+++ src/edu/gatech/at/jamespark/AdvancedItemEffects/Effects.java
@@ -1,5 +1,7 @@
 package edu.gatech.at.jamespark.AdvancedItemEffects;
 
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.EffectEffectsList;
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.PotionEffectsList;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -8,9 +10,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 // TODO Better organization & javadoc
 
@@ -21,19 +21,13 @@ import java.util.List;
  */
 public class Effects {
 
-    public final String[] potionEffectsList = {"SPEED", "SLOW",
-            "FAST_DIGGING", "SLOW_DIGGING", "INCREASE_DAMAGE", "HEAL", "HARM",
-            "JUMP", "CONFUSION", "REGENERATION", "DAMAGE_RESISTANCE",
-            "FIRE_RESISTANCE", "WATER_BREATHING", "INVISIBILITY", "BLINDNESS",
-            "NIGHT_VISION", "HUNGER", "WEAKNESS", "POISON", "WITHER",
-            "HEALTH_BOOST", "ABSORPTION", "SATURATION"};
-    public final String[] particleEffectsList = {"ENDER_SIGNAL",
-            "MOBSPAWNER_FLAMES", "POTION_BREAK", "SMOKE"};
+    private String[] potionEffectsList = PotionEffectsList.getArray();
+    private String[] particleEffectsList = EffectEffectsList.getArray();
 
     public final String POTION_KEY = "AIE_CURRENT_POTION_EFFECTS";
     public final String PARTICLE_KEY = "AIE_CURRENT_PARTICLE_EFFECTS";
 
-    public ArrayList<Player> playerList = new ArrayList<>();
+    public Set<Player> playerList = new HashSet<>();
     private Plugin plugin;
 
     public Effects(Plugin plugin) {
@@ -97,7 +91,6 @@ public class Effects {
             if (itemLore != null
                     && itemLore.contains(ChatColor.GOLD + "Effects:")) {
                 addAllItemEffects(player, itemLore);
-
             } else {
                 ItemStack heldItem = player.getItemInHand();
                 if (heldItem.hasItemMeta() && heldItem.getItemMeta().hasLore()) {
@@ -124,33 +117,31 @@ public class Effects {
             if (loreLineMatch != -1) {
                 String[] effectSplit = heldItemLore.get(loreLineMatch)
                         .replaceAll("\\s", "").split("��.");
-                if (!(playerList.contains(player))) {
-                    playerList.add(player);
-                }
+                playerList.add(player);
                 addPlayerEffectMetadata(player, PARTICLE_KEY, effectSplit[1].toUpperCase());
             }
         }
     }
 
     public void addAllBoundItemPotionEffects(Player player,
-                                             List<String> heldItemLore) {
+                                             List<String> heldItemLore) throws NumberFormatException {
+        System.out.println("POTION EFFECT ADDING");
+        String currentLoreLine;
         for (int x = 0; x < potionEffectsList.length; x++) {
-            int loreLineMatch = listContainsIgnoreCase(heldItemLore,
+            System.out.println("Checking: " + potionEffectsList[x]);
+            int matchingLineNumber = listContainsIgnoreCase(heldItemLore,
                     potionEffectsList[x]);
-            if (loreLineMatch != -1) {
-                String[] effectSplit = heldItemLore.get(loreLineMatch)
-                        .replaceAll("\\s", "").split("��.");
-                if (effectSplit.length == 3) {
-                    int multiplier = -1;
-                    try {
-                        multiplier = Integer.parseInt(effectSplit[2]);
-                    } catch (NumberFormatException e) {
-                    }
-                    if (multiplier > 0) {
-                        addItemPotionEffect(player,
-                                PotionEffectType.getByName(effectSplit[1]),
-                                multiplier);
-                    }
+            if (matchingLineNumber != -1) {
+                System.out.println("MATCH!");
+                int multiplier = -1;
+                System.out.println("LORE NAME: " + heldItemLore.get(matchingLineNumber));
+                currentLoreLine = heldItemLore.get(matchingLineNumber);
+                multiplier = Integer.parseInt(currentLoreLine.substring(currentLoreLine.lastIndexOf(ChatColor.WHITE.toString()) + 2));
+                System.out.println("multiplier:" + multiplier);
+                if (multiplier > 0) {
+                    addItemPotionEffect(player,
+                            PotionEffectType.getByName(potionEffectsList[x]),
+                            multiplier);
                 }
             }
         }
@@ -167,27 +158,25 @@ public class Effects {
      */
     public void removeAllBoundEffects(Player player) {
         if (null == player) return;
-
+        System.out.println("Removing effects");
         if (player.hasMetadata(POTION_KEY)) {
             // Get Player's active potion effects from this plugin
-            ArrayList playerPotionEffects = (ArrayList) player.getMetadata(POTION_KEY).get(0).value();
-
+            ArrayList<String> playerPotionEffects = (ArrayList) player.getMetadata(POTION_KEY).get(0).value();
+            System.out.println(playerPotionEffects + " < should be arraylist");
             // @TODO Check if all player's effects get removed
             // Clear Player's active potion effects
             // Note: We are avoiding other active potion effects not from this plugin
-            for (Object playerPotionEffect : playerPotionEffects) {
+            for (String playerPotionEffect : playerPotionEffects) {
+                System.out.println("Targeted: " + playerPotionEffects);
                 PotionEffectType potionEffectType = PotionEffectType
-                        .getByName(playerPotionEffect.toString());  // @TODO check this later
-                if (potionEffectType != null) {
-                    player.removePotionEffect(potionEffectType);
-                }
+                        .getByName(playerPotionEffect);
+                System.out.println("Removing: " + potionEffectType);
+                player.removePotionEffect(potionEffectType);
             }
-
             player.removeMetadata(POTION_KEY, plugin);
         }
-        if (player.hasMetadata(PARTICLE_KEY)) {
-            player.removeMetadata(PARTICLE_KEY, plugin);
-        }
+
+        player.removeMetadata(PARTICLE_KEY, plugin);
 
         // Remove Player from particle effect track list.
         playerList.remove(player);
@@ -201,7 +190,7 @@ public class Effects {
                                      int level) {
         player.addPotionEffect(new PotionEffect(type, 2147483647, level - 1,
                 false));
-        addPlayerEffectMetadata(player, POTION_KEY, type.toString());
+        addPlayerEffectMetadata(player, POTION_KEY, type.getName());
     }
 
     private void addAllItemEffects(Player player, List<String> itemLore) {
@@ -235,11 +224,16 @@ public class Effects {
     }
 
     private void addPlayerEffectMetadata(Player player, String key, String value) {
-        ArrayList<String> playerParticleEffects = (ArrayList) player.getMetadata(key).get(0).value();
-        if (null == playerParticleEffects) {
-            playerParticleEffects = new ArrayList<>();
-            player.setMetadata(key, new FixedMetadataValue(plugin, playerParticleEffects));
+        if (null == player || key == null || value == null || key.isEmpty() || value.isEmpty()) return;
+
+        List playerMetadata = player.getMetadata(key);
+        if (playerMetadata.isEmpty()) {
+            System.out.println("Player metadata was null.");
+            player.setMetadata(key, new FixedMetadataValue(plugin, new ArrayList<>()));
         }
-        playerParticleEffects.add(value);
+        ArrayList<String> playerEffects = (ArrayList) player.getMetadata(key).get(0).value();
+        playerEffects.add(value);
+        System.out.println("Adding new effect: " + value);
+        System.out.println("Contains?: " + ((ArrayList) player.getMetadata(key).get(0).value()).contains(value));
     }
 }
