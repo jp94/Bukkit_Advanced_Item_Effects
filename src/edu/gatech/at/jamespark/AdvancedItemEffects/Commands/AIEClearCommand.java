d52e198fd74efe8200fc9781f1d874e871d5ca28
jp94 <jamespark@gatech.edu>
2016-01-04 16:36:20 -0500

    Refactor to new effect list fetcher

--- src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIEClearCommand.java
+++ src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIEClearCommand.java
@@ -23,56 +23,47 @@ public class AIEClearCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command,
                              String label, String[] args) {
 
-        // Prevent executing from server console.
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED
                     + "Must execute this command as a player.");
             return false;
         }
-
-        // Command: aieclear
-        if (label.equalsIgnoreCase("aieclear")) {
-
-            Player player = (Player) sender;
-
-            // Check necessary cases.
-            if ((!player.hasPermission("AIE.clear")) && (!player.isOp())) {
-                player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
-                        + " lacks \"AIE.clear\" permission.");
-                return false;
-            }
-
-            if (player.getItemInHand().getItemMeta() == null) {
-                player.sendMessage(ChatColor.RED
-                        + "You must be holding an item!");
-                return false;
-            }
-
-            ItemStack item = player.getItemInHand();
-            ItemMeta itemMeta = item.getItemMeta();
-            List<String> lore = itemMeta.getLore();
-
-            if (lore != null && (lore.contains(ChatColor.GOLD + "Effects:"))) {
-
-                for (int x = lore.size() - 1; x >= effects
-                        .listContainsIgnoreCase(lore, "Effects:") && x > -1; x--) {
-                    lore.remove(x);
-                }
-
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                player.sendMessage(ChatColor.AQUA
-                        + "All effects have been removed.");
-                effects.removeAllBoundEffects(player);
-                effects.addItemEffects(player, true, true, false);
-                return true;
-
-            } else {
-                player.sendMessage(ChatColor.RED
-                        + "There are no effects to remove.");
-                return false;
-            }
+        if (!label.equalsIgnoreCase("aieclear")) return false;
+        Player player = (Player) sender;
+        if ((!player.hasPermission("AIE.clear")) && (!player.isOp())) {
+            player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
+                    + " lacks \"AIE.clear\" permission.");
+            return false;
         }
-        return false;
+        if (player.getItemInHand().getItemMeta() == null) {
+            player.sendMessage(ChatColor.RED
+                    + "You must be holding an item!");
+            return false;
+        }
+
+        ItemStack item = player.getItemInHand();
+        ItemMeta itemMeta = item.getItemMeta();
+        List<String> lore = itemMeta.getLore();
+
+        if (lore != null && (lore.contains(ChatColor.GOLD + "Effects:"))) {
+
+            for (int x = lore.size() - 1; x >= effects
+                    .listContainsIgnoreCase(lore, "Effects:") && x > -1; x--) {
+                lore.remove(x);
+            }
+
+            itemMeta.setLore(lore);
+            item.setItemMeta(itemMeta);
+            player.sendMessage(ChatColor.AQUA
+                    + "All effects have been removed.");
+            effects.removeAllBoundEffects(player);
+            effects.addItemEffects(player, true, true, false);
+            return true;
+
+        } else {
+            player.sendMessage(ChatColor.RED
+                    + "There are no effects to remove.");
+            return false;
+        }
     }
 }
--- src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIERemoveCommand.java
+++ src/edu/gatech/at/jamespark/AdvancedItemEffects/Commands/AIERemoveCommand.java
@@ -1,6 +1,8 @@
 package edu.gatech.at.jamespark.AdvancedItemEffects.commands;
 
 import edu.gatech.at.jamespark.AdvancedItemEffects.Effects;
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.EffectEffectsList;
+import edu.gatech.at.jamespark.AdvancedItemEffects.constructors.PotionEffectsList;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -22,70 +24,59 @@ public class AIERemoveCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command,
                              String label, String[] args) {
-        // Prevent executing from server console.
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED
                     + "Must execute this command as a player.");
             return false;
         }
-
-        // Command: aieremove
-        if (label.equalsIgnoreCase("aieremove")) {
-
-            Player player = (Player) sender;
-
-            // Check necessary cases.
-            if ((!player.hasPermission("AIE.remove")) && (!player.isOp())) {
-                player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
-                        + " lacks \"AIE.remove\" permission.");
-                return false;
-            }
-
-            if (player.getItemInHand().getItemMeta() == null) {
-                player.sendMessage(ChatColor.RED
-                        + "You must be holding an item!");
-                return false;
-            }
-
-            if (args.length < 1 || args.length > 2) {
-                String[] missingArgs = {
-                        ChatColor.RED + "Incomplete command input.",
-                        ChatColor.RED + "/aieremove [effect]"};
-                player.sendMessage(missingArgs);
-                return false;
-            }
-
-            if ((!effects.stringArrayContainsIgnoreCase(
-                    effects.potionEffectsList, args[0]))
-                    && !effects.stringArrayContainsIgnoreCase(
-                    effects.particleEffectsList, args[0])) {
-                player.sendMessage(ChatColor.RED + "Wrong effect type");
-                return false;
-            }
-
-            ItemStack item = player.getItemInHand();
-            ItemMeta itemMeta = item.getItemMeta();
-            List<String> lore = itemMeta.getLore();
-
-            if (lore != null && (lore.contains(ChatColor.GOLD + "Effects:"))) {
-                if (lore.size() == 2) {
-                    lore = null;
-                } else {
-                    lore.remove(effects.listContainsIgnoreCase(lore, args[0]));
-                }
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                player.sendMessage(ChatColor.AQUA
-                        + "An effect has been removed.");
-                effects.removeAllBoundEffects(player);
-                effects.addItemEffects(player, true, true, true);
-                return true;
-            } else {
-                player.sendMessage(ChatColor.RED + "There is no " + args[0]
-                        + " effect to remove.");
-                return false;
-            }
+        if (!label.equalsIgnoreCase("aieremove")) return false;
+        Player player = (Player) sender;
+        if ((!player.hasPermission("AIE.remove")) && (!player.isOp())) {
+            player.sendMessage(player.getDisplayName() + ChatColor.YELLOW
+                    + " lacks \"AIE.remove\" permission.");
+            return false;
         }
-        return false;
+        if (player.getItemInHand().getItemMeta() == null) {
+            player.sendMessage(ChatColor.RED
+                    + "You must be holding an item!");
+            return false;
+        }
+        if (args.length < 1 || args.length > 2) {
+            String[] missingArgs = {
+                    ChatColor.RED + "Incomplete command input.",
+                    ChatColor.RED + "/aieremove [effect]"};
+            player.sendMessage(missingArgs);
+            return false;
+        }
+        if ((!effects.stringArrayContainsIgnoreCase(
+                PotionEffectsList.getArray(), args[0]))
+                && !effects.stringArrayContainsIgnoreCase(
+                EffectEffectsList.getArray(), args[0])) {
+            player.sendMessage(ChatColor.RED + "Wrong effect type");
+            return false;
+        }
+
+        ItemStack item = player.getItemInHand();
+        ItemMeta itemMeta = item.getItemMeta();
+        List<String> lore = itemMeta.getLore();
+
+        if (lore != null && (lore.contains(ChatColor.GOLD + "Effects:"))) {
+            if (lore.size() == 2) {
+                lore = null;
+            } else {
+                lore.remove(effects.listContainsIgnoreCase(lore, args[0]));
+            }
+            itemMeta.setLore(lore);
+            item.setItemMeta(itemMeta);
+            player.sendMessage(ChatColor.AQUA
+                    + "An effect has been removed.");
+            effects.removeAllBoundEffects(player);
+            effects.addItemEffects(player, true, true, true);
+            return true;
+        } else {
+            player.sendMessage(ChatColor.RED + "There is no " + args[0]
+                    + " effect to remove.");
+            return false;
+        }
     }
 }
